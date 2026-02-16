b84af9597980449b05260ef2ea7499ee56ec4f70
agastyapatri <agastyapatri@fedora>
2026-02-16 07:01:03 +0530

    figure out buf_matmul

--- main.c
+++ main.c
@@ -4,56 +4,43 @@
 #include <immintrin.h>
 #include <stdlib.h>
 #include <time.h>
-#define INT 0.50
+#define INT 1
 #define ROWS INT*1024
 #define COLS INT*1024
 #define MU 0.0
 #define SIGMA 1
 #define REQUIRES_GRAD 0
 
-void matrix_map(matrix* inp, double (*func)(double)){
+matrix* matrix_map(matrix* inp, double (*func)(double)){
+	matrix* out = matrix_alloc(inp->rows, inp->cols, inp->requires_grad);
 	for(size_t i = 0; i < inp->rows; i++){
-		double* p1gradrow = inp->data + (i * inp->stride);
+		double* p1datarow = inp->data + (i * inp->stride);
+		double* o1datarow = out->data + (i * inp->stride);
 		for(size_t j = 0; j < inp->cols; j++){
-			p1gradrow[j] = func(p1gradrow[j]);
+			o1datarow[j] = func(p1datarow[j]);
 		}
 	}
+	return out;
 }
 
 
 int main(){
-	srand(time(NULL));
+	srand(0);
 
-	matrix* inp1 = matrix_ones(11, 11, 1);
-	matrix* inp2 = matrix_random_normal(11, 11, -1, 1, 1);
-	matrix* inp3 = matrix_mul(inp1, inp2);
-	matrix* out = matrix_sin(inp2);
-	// matrix* inp5 = matrix_add(inp3, inp4);
-	// matrix* out = matrix_exp(inp5);
-	matrix_grad(out);
-
-
-
-
-
-
-	for(size_t i = 0; i < out->rows; i++){
-		double* p1gradrow = out->previous[0]->grad + (i * out->stride);
-		for(size_t j = 0; j < out->cols; j++){
-			printf("%lf ", p1gradrow[j]);
-		}
-
-		printf("\n");
-	}
-	printf("\n\n");
-
-	matrix_map(inp2, dsin);
+	matrix* inp1 = matrix_random_normal(5, 5, -1, 1, 1);
+	matrix* inp2 = matrix_sigmoid(inp1);
+	matrix* out =  matrix_matmul(inp1, inp2);
 	matrix_print(inp2);
-	
 
+	// matrix* inp2 = matrix_map(inp1, dsigmoid);
+	// matrix_print(inp2);
+	// printf("\n");
+	// for(size_t i = 0; i < inp1->rows; i++){
+	// 	double* row = inp1->grad + (i * inp1->stride);
+	// 	for(size_t j = 0; j < inp1->cols; j++){
+	// 		printf("%lf ", row[j]);
+	// 	} 
+	// 	printf("\n");
+	// }
 
-
-
-
-
-}
+} 
