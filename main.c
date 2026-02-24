13fc14095b2a550bd5ec24fdb61f75447f4523af
agastyapatri <agastyapatri@fedora>
2026-02-24 19:35:34 +0530

    matmul seems to be fixed

--- main.c
+++ main.c
@@ -14,27 +14,14 @@
 #define SIGMA 1
 #define REQUIRES_GRAD 0
 
-matrix* matrix_map(matrix* inp, double (*func)(double)){
-	matrix* out = matrix_alloc(inp->rows, inp->cols, inp->requires_grad);
-	for(size_t i = 0; i < inp->rows; i++){
-		double* p1datarow = inp->data + (i * inp->stride);
-		double* o1datarow = out->data + (i * inp->stride);
-		for(size_t j = 0; j < inp->cols; j++){
-			o1datarow[j] = func(p1datarow[j]);
-		}
-	}
-	return out;
-}
-
-
-
-
 
 int main(){
 	srand(0);
-	matrix* m1 = matrix_random_normal(1, 10, 0, 1, 0);
-	matrix* m2 = matrix_random_normal(1, 10, 0, 1, 0);
-	matrix* out = matrix_transpose(m2);
-	matrix_print(m2);
-	matrix_print(out);
+	return 0;
+
 } 
+
+
+
+
+
