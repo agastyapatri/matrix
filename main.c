a97e4bb176a8beae1766dbf0fe9d2f6503da74f7
agastyapatri <agastyapatri@fedora>
2026-03-02 06:50:44 +0530

    Makefile added

--- main.c
+++ main.c
@@ -12,12 +12,14 @@
 #define INT 100
 int main(){
 	srand(0);
-	matrix* m1 = matrix_random_normal(ROWS, COLS, 0, 1, 1);
+	matrix* m1 = matrix_random_normal(ROWS, COLS+1, 0, 1, 1);
 	matrix* m2 = matrix_random_normal(ROWS, COLS, 0, 1, 1);
-	matrix* m3 = matrix_sin(m1);
-	matrix* m4 = matrix_cos(m2);
-	matrix* m5 = matrix_add(m3, m4);
-	matrix_backward(m5);
+	matrix* m3 = matrix_matmul(m1, m2);
+	// matrix* m3 = matrix_sin(m1);
+	// matrix* m4 = matrix_cos(m2);
+	// matrix* m5 = matrix_add(m3, m4);
+	// matrix_backward(m5);
+	matrix_print(m3);
 
 
 } 
